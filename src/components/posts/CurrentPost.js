ac6eec57395d872f40bf7cf8c5edb65ddb0ce3c9
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-11-10 18:24:26 +0200

    Render current post clicked

--- src/components/posts/CurrentPost.js
+++ src/components/posts/CurrentPost.js
@@ -1,4 +1,14 @@
+import { useParams } from "react-router-dom";
+import { PostContext } from '../../contexts/PostContext';
+import { useContext } from "react";
+
 export const CurrentPost = () => {
+    const { posts } = useContext(PostContext);
+    const { postId } = useParams();
+
+    const currentPost = posts.find(post => post.id === postId);
+    console.log(currentPost);
+
     return (
         <section>
             <div className="container">
@@ -6,18 +16,18 @@ export const CurrentPost = () => {
                     <div className="col-md-6">
                         <div className="img-wrapper">
                             <div className="after" />
-                            <img src="https://images.pexels.com/photos/262508/pexels-photo-262508.jpeg?cs=srgb&dl=pexels-pixabay-262508.jpg&fm=jpg" className="w-100" alt="About Us" />
+                            <img src={currentPost.imageUrl} className="w-100" alt="About Us" />
                         </div>
                     </div>
                     <div className="col-md-5">
                         <h6 className="title mb-3">
-                            Lorem ipsum dolor sit amet, consectetur.
+                            {currentPost.title}
                         </h6>
                         <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae autem
-                            rem impedit molestiae hic ducimus, consequuntur ullam dolorem
-                            quaerat beatae labore explicabo, sint laboriosam aperiam nihil
-                            inventore facilis. Quasi, facilis.
+                            {currentPost.description}
+                        </p>
+                        <p>
+                            {currentPost.content}
                         </p>
                         <a className="btn btn-outline-primary btn-sm">Edit</a>
                         <a className="btn btn-outline-primary btn-sm">Delete</a>
