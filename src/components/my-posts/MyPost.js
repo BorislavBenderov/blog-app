14d3275e7f3f2af705017117a7f2ef8f527e3253
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-11-11 09:43:39 +0200

    Render my posts and show content only on creator of the posts

---
+++ src/components/my-posts/MyPost.js
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+
+export const MyPost = ({post}) => {
+    return (
+        <div className="container">
+            <div className="row justify-content-between align-items-center">
+                <div className="col-md-6">
+                    <div className="img-wrapper">
+                        <div className="after" />
+                        <img src={post.imageUrl} className="w-100" alt="About Us" />
+                    </div>
+                </div>
+                <div className="col-md-5">
+                    <h6 className="title mb-3">
+                        {post.title}
+                    </h6>
+                    <p>
+                        {post.description}
+                    </p>
+                    <Link className="btn btn-outline-primary btn-sm" to={`/posts/${post.id}`}>Learn More</Link>
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/components/my-posts/MyPosts.js
+++ src/components/my-posts/MyPosts.js
@@ -1,28 +1,19 @@
+import { useContext } from "react";
+import { PostContext } from "../../contexts/PostContext";
+import { AuthContext } from "../../contexts/AuthContext";
+import { MyPost } from "./MyPost";
+
 export const MyPosts = () => {
+    const { posts } = useContext(PostContext);
+    const { auth } = useContext(AuthContext);
+
+    const postOwner = posts.filter(post => post.ownerId === auth.currentUser.uid);
+
     return (
         <section>
-            <div className="container">
-                <div className="row justify-content-between align-items-center">
-                    <div className="col-md-6">
-                        <div className="img-wrapper">
-                            <div className="after" />
-                            <img src="https://images.pexels.com/photos/262508/pexels-photo-262508.jpeg?cs=srgb&dl=pexels-pixabay-262508.jpg&fm=jpg" className="w-100" alt="About Us" />
-                        </div>
-                    </div>
-                    <div className="col-md-5">
-                        <h6 className="title mb-3">
-                            Lorem ipsum dolor sit amet, consectetur.
-                        </h6>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae autem
-                            rem impedit molestiae hic ducimus, consequuntur ullam dolorem
-                            quaerat beatae labore explicabo, sint laboriosam aperiam nihil
-                            inventore facilis. Quasi, facilis.
-                        </p>
-                        <button className="btn btn-outline-primary btn-sm">Learn More</button>
-                    </div> 
-                </div>
-            </div>
+            {postOwner.length > 0
+                ? postOwner.map(post => <MyPost key={post.id} post={post} />)
+                : <p>No posts in database!</p>}
         </section>
     );
 }
