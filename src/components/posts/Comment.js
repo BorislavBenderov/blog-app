47f030fc3458f3600e3b593e4c6bc5f52ea76e0d
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-11-13 20:39:38 +0200

    Add possibility users to delete comments

--- src/components/posts/Comment.js
+++ src/components/posts/Comment.js
@@ -1,7 +1,26 @@
-import { AuthContext } from "../../contexts/AuthContext";
-import { useContext } from "react";
+import { doc, deleteDoc } from 'firebase/firestore';
+import { database } from '../../firebaseConfig';
+import { AuthContext } from '../../contexts/AuthContext';
+import { useContext } from 'react';
 
-export const Comment = ({comment}) => {
+export const Comment = ({ comment }) => {
+    const { auth } = useContext(AuthContext);
+    let commentOwner = null;
+
+    if (auth.currentUser) {
+        commentOwner = comment.uid === auth.currentUser.uid;
+    }
+
+    const onDeleteComment = async (id, e) => {
+        const confirmation = window.confirm('Are you sure you want to delete this comment?');
+
+        if (confirmation) {
+            e.preventDefault();
+            await deleteDoc(doc(database, 'comments', id));
+        }
+    }
+
+
     return (
         <li className="comments-box">
             <h6>
@@ -10,6 +29,10 @@ export const Comment = ({comment}) => {
             <p>
                 {comment.text}
             </p>
+            {commentOwner
+                ? <button className="comments-button" onClick={(e) => onDeleteComment(comment.id, e)}>Delete</button>
+                : ''}
+
         </li>
     );
 }
