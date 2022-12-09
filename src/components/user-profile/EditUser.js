e8e70767619a28aaa1655c6679b783aa28206cb8
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-09 08:17:42 +0200

    Render errors on the screen in edit user component

--- src/components/user-profile/EditUser.js
+++ src/components/user-profile/EditUser.js
@@ -7,6 +7,7 @@ import { database } from "../../firebaseConfig";
 import { useNavigate } from "react-router-dom";
 
 export const EditUser = () => {
+    const [err, setErr] = useState('');
     const { users } = useContext(UserContext);
     const { loggedUser } = useContext(AuthContext);
     const navigate = useNavigate();
@@ -25,17 +26,17 @@ export const EditUser = () => {
         const isUsernameInUse = users.filter(user => user.uid !== loggedUser.uid).find(user => user.username === username);
 
         if (isUsernameInUse) {
-            alert('This username is already in use!');
+            setErr('This username is already in use!');
             return;
         }
 
         if (username === '') {
-            alert('Please set a username!');
+            setErr('Please set a username!');
             return;
         }
 
         if (username.length < 2 || username.length > 10) {
-            alert('Username must be more than 2 characters and less then 10!');
+            setErr('Username must be more than 2 characters and less then 10!');
             return;
         }
 
@@ -49,7 +50,7 @@ export const EditUser = () => {
                 navigate('/');
             })
             .catch((err) => {
-                alert(err.message);
+                setErr(err.message);
             })
     }
 
@@ -65,6 +66,7 @@ export const EditUser = () => {
             value={value.username} 
             onChange={(e) => setValue(state => ({...state, [e.target.name]: e.target.value}))} />
             <button type="submit">Edit</button>
+            <p className="error">{err}</p>
         </form>
     );
 }
