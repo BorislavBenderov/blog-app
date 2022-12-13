54e8f3a0f748ed8e04ca92f0ca1f7f7f2858bb97
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-13 08:02:00 +0200

    Refactor context and router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import { AuthContextProvider } from './contexts/AuthContext';
+import { PostContextProvider } from './contexts/PostContext';
+import { UserContextProvider } from './contexts/UserContext';
 import { Nav } from './components/nav/Nav';
 import { Header } from './components/header/Header';
 import { Login } from './components/auth/Login';
@@ -16,26 +19,30 @@ import { ProtectedRoutes } from './ProtectedRoutes';
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Nav />
-        <Header />
-        <main className='site__content'>
-          <Routes>
-            <Route path='*' element={<NotFound />} />
-            <Route path='/' element={<Posts />} />
-            <Route path='/posts/:postId' element={<CurrentPost />} />
-            <Route path='/user/edit/:userId' element={<EditUser />} />
-            <Route path='/register' element={<Register />} />
-            <Route path='/login' element={<Login />} />
-            <Route element={<ProtectedRoutes />}>
-              <Route path='/myposts' element={<MyPosts />} />
-              <Route path='/create' element={<Create />} />
-              <Route path='/edit/:postId' element={<Edit />} />
-            </Route>
-          </Routes>
-        </main>
-        <Footer />
-      </BrowserRouter>
+      <AuthContextProvider>
+        <PostContextProvider>
+          <UserContextProvider>
+            <Nav />
+            <Header />
+            <main className='site__content'>
+              <Routes>
+                <Route path='*' element={<NotFound />} />
+                <Route path='/' element={<Posts />} />
+                <Route path='/posts/:postId' element={<CurrentPost />} />
+                <Route path='/user/edit/:userId' element={<EditUser />} />
+                <Route path='/register' element={<Register />} />
+                <Route path='/login' element={<Login />} />
+                <Route element={<ProtectedRoutes />}>
+                  <Route path='/myposts' element={<MyPosts />} />
+                  <Route path='/create' element={<Create />} />
+                  <Route path='/edit/:postId' element={<Edit />} />
+                </Route>
+              </Routes>
+            </main>
+            <Footer />
+          </UserContextProvider>
+        </PostContextProvider>
+      </AuthContextProvider>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { AuthContextProvider } from './contexts/AuthContext';
-import { PostContextProvider } from './contexts/PostContext';
 import App from './App';
 import 'font-awesome/css/font-awesome.css';
-import { UserContextProvider } from './contexts/UserContext';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <AuthContextProvider>
-        <PostContextProvider>
-            <UserContextProvider>
-                <App />
-            </UserContextProvider>
-        </PostContextProvider>
-    </AuthContextProvider>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 );
